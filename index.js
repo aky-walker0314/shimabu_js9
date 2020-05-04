159e7e97989271fdfd2e9117b1ca0f4673b2e146
aky-walker0314 <50131017+aky-walker0314@users.noreply.github.com>
2020-05-05 08:04:35 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const lists = document.getElementById("lists");
 
 button.addEventListener("click",async function(){
     //データのやり取り
-    // const res =await fetch("https://jsonplaceholder.typicode.com/users");
-    //const users = await res.json();
-    //console.log(users);
+     const res =await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+    console.log(users);
 
-    //DOm操作
-    const list= document.createElement("li");
-    list.innerText="yeah";
-    lists.appendChild(list);
-    console.log(list);
+    //DOM操作
+    users.forEach(function(user) {
+        console.log(user);
+    });
+    //const list= document.createElement("li");
+    //list.innerText="yeah";
+    //lists.appendChild(list);
+    //console.log(list);
 
 });
