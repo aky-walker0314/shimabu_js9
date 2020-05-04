ec51c16da379a61e7944162461c5fd41192f78d5
aky-walker0314 <50131017+aky-walker0314@users.noreply.github.com>
2020-05-05 06:58:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const lists = document.getElementById("lists");
 button.addEventListener("click",async function(){
     const res =await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await res.json();
+    console.log(users);
 });
