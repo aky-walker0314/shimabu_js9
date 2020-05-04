f9d0f5f06426a55376348014cfcd2cf90c9100d3
aky-walker0314 <50131017+aky-walker0314@users.noreply.github.com>
2020-05-05 06:51:41 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const button = document.getElementById("addBtn");
 const lists = document.getElementById("lists");
 
 button.addEventListener("click",async function(){
-    const res = await fetch(
-        const res =await fetch("https://jsonplaceholder.typicode.com/users");
+    const res =await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
 });
